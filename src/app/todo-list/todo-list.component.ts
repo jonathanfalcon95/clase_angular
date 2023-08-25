89e99da3fd269ae4995a1fecb479dd398bef7d9a
jonathanfalcon95 <jonathan.falcon.95@hotmail.com>
2023-08-25 10:52:25 -0400

    first commit

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss']
+})
+export class TodoListComponent implements OnInit {
+
+  tasks: string[] = [];
+  newTask: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  addTask() {
+    if (this.newTask.trim() !== '') {
+      this.tasks.push(this.newTask.trim());
+      this.newTask = '';
+    }
+  }
+
+  removeTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
+
+}
